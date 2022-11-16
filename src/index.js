923d41ef63b80de97e92f711c928b169ec5d0ce0
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-15 21:15:52 -0300

    Fix delete repository

--- src/index.js
+++ src/index.js
@@ -56,11 +56,11 @@ app.put('/repositories/:id', (request, response) => {
 app.delete('/repositories/:id', (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(
+  const repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
   );
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: 'Repository not found' });
   }
 
